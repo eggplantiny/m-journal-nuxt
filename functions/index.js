8be4897e77ee78d17234d38daff09bd61c844caf
eggplantiny <eggplantiny@gmail.com>
2020-12-15 22:14:06 +0900

    firebase/function 관련 작업

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text
+  const writeResult = await admin.firestore().collection('messages').add({original: original});
+  res.json({ result: `Message with ID: ${writeResult.id} added.` })
+})
