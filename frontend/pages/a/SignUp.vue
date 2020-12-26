924810be07b0240fa305e7bbdf18770b8580337d
eggplantiny <eggplantiny@gmail.com>
2020-12-26 17:56:25 +0900

    (#2) 로그인 및 회원가입 기능 개발 완료

--- frontend/pages/a/SignUp.vue
+++ frontend/pages/a/SignUp.vue
@@ -1,7 +1,35 @@
 <template>
   <v-row>
-    <v-col cols="6" offset="3">
-
+    <v-col cols="12">
+      <v-card
+        flat
+        outlined
+        max-width="400"
+        class="mx-auto"
+      >
+        <v-card-title class="justify-center">
+          회원가입
+        </v-card-title>
+        <div class="px-4 mb-4">
+          <v-text-field
+            flat
+            outlined
+            solo
+            hide-details="auto"
+            label="닉네임"
+          />
+        </div>
+        <v-card-actions class="px-4">
+          <v-btn
+            color="primary"
+            large
+            block
+            outlined
+          >
+            확인
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-col>
   </v-row>
 </template>
