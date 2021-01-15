79045f893e4f184c624998442f4b49d3b3cd7ab8
eggplantiny <eggplantiny@gmail.com>
2021-01-15 20:05:57 +0900

    [Frontend] Settings 구현 #42 - 닉네임 입력 폼 작성 - 일기 자세히 보기 폼 작성 - 다크모드 폼 작성 - 기본 기능 설정 및 API 연결

--- frontend/pages/Setting/index.vue
+++ frontend/pages/Setting/index.vue
@@ -1,41 +1,48 @@
 <template>
   <v-row justify="center">
-    <v-col
-      cols="12"
-      sm="12"
-      class="py-0"
+    <v-form
+      ref="form"
+      class="setting-form"
+      lazy-validation
     >
-      <v-text-field
-        v-model="inputs.nickName"
-        label="닉네임"
-        outlined
-        solo
-        flat
-        hide-details="auto"
-      />
-    </v-col>
-    <v-col
-      cols="12"
-      sm="12"
-      class="py-0"
-    >
-      <v-checkbox
-        v-model="inputs.detail"
-        label="일기 자세히보기"
-        hide-details="auto"
-      />
-    </v-col>
-    <v-col
-      cols="12"
-      sm="12"
-      class="py-0"
-    >
-      <v-checkbox
-        v-model="inputs.dark"
-        label="다크모드"
-        hide-details="auto"
-      />
-    </v-col>
+      <v-col
+        cols="12"
+        sm="12"
+        class="py-0"
+      >
+        <v-text-field
+          v-model="inputs.nickName"
+          label="닉네임"
+          outlined
+          solo
+          flat
+          hide-details="auto"
+          :rules="rules.required"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        sm="12"
+        class="py-0"
+      >
+        <v-checkbox
+          v-model="inputs.detail"
+          label="일기 자세히보기"
+          hide-details="auto"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        sm="12"
+        class="py-0"
+      >
+        <v-checkbox
+          v-model="inputs.dark"
+          label="다크모드"
+          hide-details="auto"
+        />
+      </v-col>
+    </v-form>
     <v-col cols="12">
       <v-btn
         class="mt-8"
@@ -43,6 +50,7 @@
         dark
         large
         color="indigo lighten-2"
+        @click="submit"
       >
         저장
       </v-btn>
@@ -54,18 +62,57 @@
 export default {
   name: 'Setting',
   layout: 'app',
+  middleware: ['auth'],
   data () {
     return {
       inputs: {
         nickName: '',
         dark: false,
-        detail: false
+        detail: false,
+        color: ''
+      },
+      rules: {
+        required: [
+          v => !!v || '입력해주세요 😉'
+        ]
       }
+    }
+  },
+  beforeMount () {
+    const userInfo = this.$store.getters['auth/user']
+    const { dark, nickName, detail, color } = userInfo
+
+    this.inputs.nickName = nickName
+    this.inputs.dark = dark === true
+    this.inputs.detail = detail === true
+    this.inputs.color = color
+  },
+  methods: {
+    async submit () {
+      const valid = await this.$refs.form.validate()
+
+      if (!valid) {
+        return
+      }
+
+      const { nickName, dark, detail, color } = this.inputs
+
+      try {
+        await this.$axios.$put('/settings', {
+          nickName, dark, detail, color
+        })
+      } catch (e) {
+        return this.$dialog.notify.error(e)
+      }
+
+      this.$router.go(-1)
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.setting-form {
+  width: 100%;
+}
 </style>
